ce48d9f5e229c7fdbc9c9050e0172b4aee54dd98
Leila Conti <leilaconti@outlook.com>
2023-08-17 16:34:57 +0100

    home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Header } from '../../components/Header/Header';
+import { HeroImage } from '../../components/HeroImage/HeroImage';
+import { SocialButtons } from '../../components/SocialButtons/SocialButtons';
+import './Home.scss';
+
+export const Home = () => {
+    return (
+        <>
+            <Header />
+            <div className="heroImageBackground">
+                <HeroImage /> 
+                <SocialButtons />
+            </div>
+        </>
+    );
+};
