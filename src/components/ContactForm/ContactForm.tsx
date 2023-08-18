38ccb930cecefeb2b4e0a190ac8338b5be3a998e
Leila Conti <leilaconti@outlook.com>
2023-08-18 20:03:48 +0100

    contact form component

---
+++ src/components/ContactForm/ContactForm.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ContactForm.scss';
+
+export const ContactForm = () => {
+    return (
+        <form className='contactForm'>
+            <label className='fieldLabel'>First name:</label>
+            <input className='inputFields' type="text" id="fname" name="fname" />
+            <label className='fieldLabel'>Last name:</label>
+            <input className='inputFields' type="text" id="lname" name="lname"/>
+            <label className='fieldLabel'>Message:</label>
+            <input className='inputFields' type="text" id="lname" name="lname" />
+            <label className='fieldLabel'>Your email:</label>
+            <input className='inputFields' type="text" id="lname" name="lname" />
+            <input className='submitButton' type="submit" value="Submit" />
+        </form>
+    );
+};
