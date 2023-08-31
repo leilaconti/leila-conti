55dc8499c171823c32edbcb653b7e9678cfb829e
Leila Conti <leilaconti@outlook.com>
2023-08-31 09:58:03 +0100

    wip

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Header } from '../../components/Header/Header';
 import { HeroImage } from '../../components/HeroImage/HeroImage';
 import { SocialButtons } from '../../components/SocialButtons/SocialButtons';
 import './Home.scss';
+import { Contact } from '../Contact/Contact';
 
 export const Home = () => {
+    const ref = useRef(null);
+
+    const handleClick = () => {
+        ref.current?.scrollIntoView({ behavior: 'smooth' });
+    };
+
     return (
         <>
             <Header />
-            <div className="heroImageBackground">
+            <div className="containerHome">
+                {/* <button onClick={handleClick} style={{ width: '120px' }}>
+                    About Me
+                </button> */}
+
                 <HeroImage />
-                <SocialButtons />
             </div>
+
+            {/* <h1 ref={ref}>About Me</h1> */}
+            {/* <Contact /> */}
         </>
     );
 };
