4fa195e4e99666b7e4bb236e8c3d95ee66333bbc
Leila Conti <leilaconti@outlook.com>
2023-08-31 09:52:31 +0100

    remove home button

--- src/components/HeroImage/HeroImage.tsx
+++ src/components/HeroImage/HeroImage.tsx
@@ -1,28 +1,40 @@
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
 import './HeroImage.scss';
+import avatar from './avatar.jpg';
+import { SocialButtons } from '../SocialButtons/SocialButtons';
 
 export const HeroImage = () => {
-    return (
-        <div>
-            <TypeAnimation
-                sequence={["Hi There, I'm Leila Conti", 1000]}
-                wrapper="span"
-                speed={50}
-                style={{
-                    fontSize: '3em',
-                    fontFamily: 'monospace',
-                    display: 'inline-block'
-                }}
-                repeat={Infinity}
-            />
 
-            <p style={{ fontFamily: 'monospace', fontSize: '18px' }}>
-                Software Developer
-            </p>
-            <button style={{ fontFamily: 'monospace', fontSize: '14px' }}>
-                About me ↓
-            </button>
-        </div>
+    return (
+        <>
+            <div className="container">
+                <div className="one">
+                    <TypeAnimation
+                        sequence={["Hi There, I'm Leila Conti", 1000]}
+                        wrapper="span"
+                        speed={50}
+                        style={{
+                            fontSize: '3em',
+                            fontFamily: 'monospace',
+                            display: 'inline-block'
+                        }}
+                        repeat={Infinity}
+                    />
+                    <div>
+                        <p
+                            style={{
+                                fontFamily: 'monospace',
+                                fontSize: '18px'
+                            }}
+                        >
+                            Software Developer
+                        </p>
+                        <SocialButtons />
+                    </div>
+                </div>
+                <img className="two" src={avatar} alt="avatar" />
+            </div>
+        </>
     );
 };
